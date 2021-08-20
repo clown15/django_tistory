5abd2c09d9c7f79a0da6dc2fb0ee68a82fc1d236
clown15 <thsdhrhd15@naver.com>
2021-08-20 07:34:07 +0000

    DOCS install Pillow

--- blogs/models.py
+++ blogs/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from helpers import BaseModel
+from helpers.models import BaseModel
 from users.models import User
 
 # Create your models here.
@@ -8,4 +8,4 @@ class Post(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255, blank=False)
     content = models.TextField()
-    image = models.ImageField(blank=True, numm=True)
+    image = models.ImageField(blank=True, null=True)
