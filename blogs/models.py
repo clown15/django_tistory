1f705b593f9fb7a67f439c7c60acf0c6c9122155
clown15 <thsdhrhd15@naver.com>
2021-08-27 06:05:54 +0000

    Style add comments

--- blogs/models.py
+++ blogs/models.py
@@ -15,6 +15,8 @@ class Post(BaseModel):
         return "%s" % (self.title)
 
 class Comment(BaseModel):
+    # user 와 post의 경우 django에서 자동으로 _id를 추가해 columm을 생성한다.
+    # 원하는 이름으로 생성하기 위해서는 db_column='my_column_name'을 추가해 원하는 이름으로 column을 생성 가능하다.
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.TextField()
